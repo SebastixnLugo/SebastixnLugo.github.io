70f1e7383c604ca9b16cc009d0aa69055450e617
Sebastian Lugo <sebxstianlugo@gmail.com>
2022-05-28 08:07:28 -0300

    últimos cambios realizados antes del veredicto

---
+++ js/form.js
@@ -0,0 +1,17 @@
+debugger
+alert("Por favor, ingrese su nombre de usuario y contraseña");
+
+let = username("CoderHouse");
+let = password("12345");
+
+if ((username == "CoderHouse") || (username == "coderhouse")){
+    console.log("Bienvenido a Trust", username);
+} else if((username != "Coderhouse") || (username == "coderhouse")) {
+    console.ward("El usuario ingresado es incorrecto.");
+}
+
+if (password == "12345") {
+    console.log("La contraseña es correcta, ¡bienvenido!")
+} else if (password != "12345"){
+    console.warn("Su contraseña es incorrecta")
+}
